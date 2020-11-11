c0f9e3fb47e36f4265d3593746362c4f45763cc1
bitsory <rangdad@gmail.com>
2020-11-11 12:09:38 -0800

    Updates

---
+++ precache-manifest.f52a9cf66840b59a889e891f38940d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e924199e07df4749d8d01c584130bdc9",
+    "url": "/habit-tracker/index.html"
+  },
+  {
+    "revision": "56ae7c95fd9dc3ff66e6",
+    "url": "/habit-tracker/static/css/main.4c7b494d.chunk.css"
+  },
+  {
+    "revision": "4c34959520e3c645f65f",
+    "url": "/habit-tracker/static/js/2.fbf8ba2c.chunk.js"
+  },
+  {
+    "revision": "a93ce933847b2bcd16bb222ad89e237b",
+    "url": "/habit-tracker/static/js/2.fbf8ba2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ae7c95fd9dc3ff66e6",
+    "url": "/habit-tracker/static/js/main.dd3cec70.chunk.js"
+  },
+  {
+    "revision": "c6331170567cdc886983",
+    "url": "/habit-tracker/static/js/runtime-main.50cbc135.js"
+  }
+]);
